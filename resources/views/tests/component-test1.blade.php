7ac7d99d265e19154d43eeb6e789a8f5f7b4cb9d
takuto0607 <mnb.jennifer@gmail.com>
2024-08-02 00:29:43 +0900

    名前付きスロット

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,3 +1,7 @@
 <x-tests.app>
+  <x-slot name="header">
+    ヘッダー1
+  </x-slot>
+
   コンポーネントテストテスト1
 </x-tests.app>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,3 +1,7 @@
 <x-tests.app>
+  <x-slot name="header">
+    ヘッダー2
+  </x-slot>
+
   コンポーネントテストテスト2
 </x-tests.app>
