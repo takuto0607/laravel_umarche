0461734522750229ab3b6b898617a30b5efcad75
takuto0607 <mnb.jennifer@gmail.com>
2024-08-17 22:39:17 +0900

    intervention/image

--- app/Http/Controllers/Owner/ShopController.php
+++ app/Http/Controllers/Owner/ShopController.php
@@ -8,6 +8,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+use InterventionImage;
 
 class ShopController extends Controller
 {
@@ -55,7 +56,19 @@ class ShopController extends Controller
     {
         $imageFile = $request->image;
         if (!is_null($imageFile) && $imageFile->isValid()) {
-            Storage::putFile('public/shops', $imageFile);
+            // リサイズなしの場合
+            // Storage::putFile('public/shops', $imageFile);
+
+            // リサイズあり
+            $fileName = uniqid(rand().'_');
+            $extension = $imageFile->extension();
+            $fileNameToStore = $fileName . '.' . $extension;
+
+            $resizedImage = InterventionImage::make($imageFile)->resize(1920, 1080)->encode();
+
+            // dd($imageFile, $resizedImage);
+
+            Storage::put('public/shops/' . $fileNameToStore, $resizedImage);
         }
 
         return redirect()->route('owner.shops.index');
