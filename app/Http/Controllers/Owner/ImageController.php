e7358ee9756fcfd604eda4a246d487e3e3fd24a5
takuto0607 <mnb.jennifer@gmail.com>
2024-08-18 02:49:15 +0900

    image index

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -3,16 +3,42 @@
 namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
+use App\Models\Image;
+use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ImageController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth:owners');
+
+        $this->middleware(function (Request $request, Closure $next) {
+            $id = $request->route()->parameter('image');
+
+            if (!is_null($id)) {
+                $imagesOwnerId = Image::findOrFail($id)->owner->id;
+                $imageId = (int)$imagesOwnerId;
+
+                if ($imageId !== Auth::id()) {
+                    // 404画面
+                    abort(404);
+                }
+            }
+
+            return $next($request);
+        });
+    }
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        //
+        $images = Image::where('owner_id', Auth::id())->orderBy('updated_at', 'desc')->paginate(20);
+
+        return view('owner.images.index', compact('images'));
     }
 
     /**
