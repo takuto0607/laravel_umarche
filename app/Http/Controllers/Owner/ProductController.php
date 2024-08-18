9ca449156218f9f8c898396058a129e03b77bb98
takuto0607 <mnb.jennifer@gmail.com>
2024-08-18 23:27:10 +0900

    category

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -6,7 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Models\Owner;
 use App\Models\Image;
 use App\Models\Product;
-use App\Models\SecondaryCategory;
+use App\Models\Shop;
+use App\Models\PrimaryCategory;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -58,7 +59,11 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        $shops = Shop::where('owner_id', Auth::id())->select('id', 'name')->get();
+        $images = Image::where('owner_id', Auth::id())->select('id', 'title', 'filename')->orderBy('updated_at', 'desc')->get();
+        $categories = PrimaryCategory::with('secondary')->get();
+
+        return view('owner.products.create', compact('shops', 'images', 'categories'));
     }
 
     /**
