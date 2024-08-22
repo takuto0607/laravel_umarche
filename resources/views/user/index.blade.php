c52633dd5eb1eaa0022c90025b8c531da2a41a1e
takuto0607 <mnb.jennifer@gmail.com>
2024-08-23 00:03:39 +0900

    view調整

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -15,8 +15,10 @@
               <a href="">
                 <div class="border rounded-md p-2 md:p-4">
                   <x-thumbnail filename="{{$product->imageFirst->filename ?? ''}}" type="products" />
-                  <div class="text-white-700">
-                    {{ $product->name }}
+                  <div class="mt-4">
+                    <h3 class="text-xs tracking-widest title-font mb-1">{{ $product->category->name }}</h3>
+                    <h2 class="title-font text-lg font-medium">{{ $product->name }}</h2>
+                    <p class="mt-1">{{ number_format($product->price) }}<span class="text-sm"> 円（税込）</span></p>
                   </div>
                 </div>
               </a>
