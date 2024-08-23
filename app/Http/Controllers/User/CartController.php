c4eb41f252b8dd75974d229e51128d1832a97793
takuto0607 <mnb.jennifer@gmail.com>
2024-08-24 03:44:19 +0900

    cart index

--- app/Http/Controllers/User/CartController.php
+++ app/Http/Controllers/User/CartController.php
@@ -3,13 +3,27 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use App\Models\Cart;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class CartController extends Controller
 {
-    public function add(Request $request)
+    public function index ()
+    {
+        $user = User::findOrFail(Auth::id());
+        $products = $user->products;
+        $totalPrice = 0;
+
+        foreach ($products as $product) {
+            $totalPrice += $product->price * $product->pivot->quantity;
+        }
+
+        return view('user.cart', compact('products', 'totalPrice'));
+    }
+
+    public function add (Request $request)
     {
         $itemInCart = Cart::where('product_id', $request->product_id)->where('user_id', Auth::id())->first();
 
@@ -24,6 +38,6 @@ class CartController extends Controller
             ]);
         }
         
-        dd('テスト');
+        return redirect()->route('user.cart.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,9 @@ Route::middleware('auth:users')->group(function () {
 });
 
 Route::prefix('cart')->middleware('auth:users')->group(function () {
+    Route::get('/', [CartController::class, 'index'])
+                ->name('cart.index');
+
     Route::post('add', [CartController::class, 'add'])
                 ->name('cart.add');
 });
