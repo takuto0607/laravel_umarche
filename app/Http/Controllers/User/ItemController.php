fc074495c544bcae9950f88d8e4c63aff899817d
takuto0607 <mnb.jennifer@gmail.com>
2024-08-24 23:45:42 +0900

    コンストラクタの修正

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use App\Models\Stock;
+use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -13,6 +14,21 @@ class ItemController extends Controller
     public function __construct()
     {
         $this->middleware('auth:users');
+
+        $this->middleware(function (Request $request, Closure $next) {
+            $id = $request->route()->parameter('item');
+
+            if (!is_null($id)) {
+                $itemId = Product::availableItems()->where('products.id', $id)->exists();
+
+                if (!$itemId) {
+                    // 404画面
+                    abort(404);
+                }
+            }
+
+            return $next($request);
+        });
     }
 
     public function index ()
